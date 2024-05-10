c00ce9aa8658be3d75efa2f8ce7589909c30cd51
Laurent Aerens <Laurent.Aerens@codit.eu>
2024-05-10 14:38:12 +0200

    added Hello World lib

---
+++ HelloWorld/helloWord.cs
@@ -0,0 +1,18 @@
+﻿using System.Runtime.CompilerServices;
+
+namespace HelloWorld
+{
+    
+    public class helloWord
+    {
+        private static string hello = "Hello World!";
+        public helloWord()
+        {
+        }
+        public static string GetHello()
+        {
+            return hello;
+        }
+    }
+
+}
