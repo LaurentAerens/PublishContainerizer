add46c9f500c5bfe60aa95ee415ceb2370ee5861
Laurent Aerens <Laurent.Aerens@codit.eu>
2024-06-05 10:38:09 +0200

    added globalization case

---
+++ GlobalTestConsoleApp/Program.cs
@@ -0,0 +1,17 @@
+﻿using System.Globalization;
+
+namespace GlobalTestConsoleApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var cultures = CultureInfo.GetCultures(CultureTypes.AllCultures);
+            foreach (var culture in cultures)
+            {
+                System.Console.WriteLine(culture.Name);
+            }
+            
+        }
+    }
+}
